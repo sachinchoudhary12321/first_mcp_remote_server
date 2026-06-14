4904b548c98c14ad8876835427d2eb47bb5c904a
sachin choudhary <amisachinchoudhary@gmail.com>
2026-06-14 18:31:36 +0530

    add sync for betterment of multiple user

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastmcp import FastMCP
 import os
-import sqlite3
+import aiosqlite
+import tempfile
+import asyncio
 
 DB_PATH = os.path.join(os.path.dirname(__file__), "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
 mcp = FastMCP("ExpenseTracker")
 
-def init_db():
+async def init_db():
     with sqlite3.connect(DB_PATH) as c:
         c.execute("""
             CREATE TABLE IF NOT EXISTS expenses(
@@ -23,9 +25,9 @@ def init_db():
 init_db()
 
 @mcp.tool()
-def add_expense(date, amount, category, subcategory="", note=""):
+async def add_expense(date, amount, category, subcategory="", note=""):# async is use for the differnt user use together
     '''Add a new expense entry to the database.'''
-    with sqlite3.connect(DB_PATH) as c:
+    with aiosqlite.connect(DB_PATH) as c:
         cur = c.execute(
             "INSERT INTO expenses(date, amount, category, subcategory, note) VALUES (?,?,?,?,?)",
             (date, amount, category, subcategory, note)
@@ -33,9 +35,9 @@ def add_expense(date, amount, category, subcategory="", note=""):
         return {"status": "ok", "id": cur.lastrowid}
     
 @mcp.tool()
-def list_expenses(start_date, end_date):
+async def list_expenses(start_date, end_date):
     '''List expense entries within an inclusive date range.'''
-    with sqlite3.connect(DB_PATH) as c:
+    with aiosqlite.connect(DB_PATH) as c:
         cur = c.execute(
             """
             SELECT id, date, amount, category, subcategory, note
@@ -49,9 +51,9 @@ def list_expenses(start_date, end_date):
         return [dict(zip(cols, r)) for r in cur.fetchall()]
 
 @mcp.tool()
-def summarize(start_date, end_date, category=None):
+async def summarize(start_date, end_date, category=None):
     '''Summarize expenses by category within an inclusive date range.'''
-    with sqlite3.connect(DB_PATH) as c:
+    with aiosqlite.connect(DB_PATH) as c:
         query = (
             """
             SELECT category, SUM(amount) AS total_amount
@@ -72,7 +74,7 @@ def summarize(start_date, end_date, category=None):
         return [dict(zip(cols, r)) for r in cur.fetchall()]
 
 @mcp.resource("expense://categories", mime_type="application/json")
-def categories():
+async def categories():
     # Read fresh each time so you can edit the file without restarting
     with open(CATEGORIES_PATH, "r", encoding="utf-8") as f:
         return f.read()
